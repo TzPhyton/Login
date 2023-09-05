26cf81fdf3d521b2360f1b4072aff7495042de0f
Goyo <dur123en@gmail.com>
2023-09-05 17:46:11 -0500

    Initial Commit

---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Enrique on 5/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
